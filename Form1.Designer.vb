64bae737972a8a5dbbddb7df6cf2ec19f72e0415
eldarius <caltar8@gmail.com>
2022-03-14 00:07:07 +0100

    Maj nom btn + Ajout comportement Discord/WebSite

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form remplace la méthode Dispose pour nettoyer la liste des composants.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,159 +20,168 @@ Partial Class Form1
     'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
     'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
     'Ne la modifiez pas à l'aide de l'éditeur de code.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.FormSkin1 = New Imperial_Life.FormSkin()
-        Me.FlatClose1 = New Imperial_Life.FlatClose()
-        Me.FlatMini1 = New Imperial_Life.FlatMini()
-        Me.FlatButton1 = New Imperial_Life.FlatButton()
-        Me.FlatButton2 = New Imperial_Life.FlatButton()
-        Me.FlatProgressBar1 = New Imperial_Life.FlatProgressBar()
-        Me.FlatButton3 = New Imperial_Life.FlatButton()
-        Me.FlatButton4 = New Imperial_Life.FlatButton()
-        Me.FormSkin1.SuspendLayout()
+        Me.BackGround = New Imperial_Life.FormSkin()
+        Me.BtnDiscord = New Imperial_Life.FlatButton()
+        Me.BtnWebSite = New Imperial_Life.FlatButton()
+        Me.ProgBarDownloadMod = New Imperial_Life.FlatProgressBar()
+        Me.BtnPlay = New Imperial_Life.FlatButton()
+        Me.BtnMaj = New Imperial_Life.FlatButton()
+        Me.BtnMini = New Imperial_Life.FlatMini()
+        Me.BtnClose = New Imperial_Life.FlatClose()
+        Me.BackGround.SuspendLayout()
         Me.SuspendLayout()
         '
-        'FormSkin1
+        'BackGround
         '
-        Me.FormSkin1.BackColor = System.Drawing.Color.White
-        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
-        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
-        Me.FormSkin1.Controls.Add(Me.FlatButton4)
-        Me.FormSkin1.Controls.Add(Me.FlatButton3)
-        Me.FormSkin1.Controls.Add(Me.FlatProgressBar1)
-        Me.FormSkin1.Controls.Add(Me.FlatButton2)
-        Me.FormSkin1.Controls.Add(Me.FlatButton1)
-        Me.FormSkin1.Controls.Add(Me.FlatMini1)
-        Me.FormSkin1.Controls.Add(Me.FlatClose1)
-        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
-        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
-        Me.FormSkin1.HeaderMaximize = False
-        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
-        Me.FormSkin1.Name = "FormSkin1"
-        Me.FormSkin1.Size = New System.Drawing.Size(800, 450)
-        Me.FormSkin1.TabIndex = 0
-        Me.FormSkin1.Text = "Launcher Imperial Life"
+        Me.BackGround.BackColor = System.Drawing.Color.White
+        Me.BackGround.BaseColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.BackGround.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.BackGround.Controls.Add(Me.BtnDiscord)
+        Me.BackGround.Controls.Add(Me.BtnWebSite)
+        Me.BackGround.Controls.Add(Me.ProgBarDownloadMod)
+        Me.BackGround.Controls.Add(Me.BtnPlay)
+        Me.BackGround.Controls.Add(Me.BtnMaj)
+        Me.BackGround.Controls.Add(Me.BtnMini)
+        Me.BackGround.Controls.Add(Me.BtnClose)
+        Me.BackGround.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.BackGround.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.BackGround.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.BackGround.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.BackGround.HeaderMaximize = False
+        Me.BackGround.Location = New System.Drawing.Point(0, 0)
+        Me.BackGround.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.BackGround.Name = "BackGround"
+        Me.BackGround.Size = New System.Drawing.Size(600, 488)
+        Me.BackGround.TabIndex = 0
+        Me.BackGround.Text = "Launcher Imperial Life"
         '
-        'FlatClose1
+        'BtnDiscord
         '
-        Me.FlatClose1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.FlatClose1.BackColor = System.Drawing.Color.White
-        Me.FlatClose1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
-        Me.FlatClose1.Font = New System.Drawing.Font("Marlett", 10.0!)
-        Me.FlatClose1.Location = New System.Drawing.Point(770, 12)
-        Me.FlatClose1.Name = "FlatClose1"
-        Me.FlatClose1.Size = New System.Drawing.Size(18, 18)
-        Me.FlatClose1.TabIndex = 0
-        Me.FlatClose1.Text = "FlatClose1"
-        Me.FlatClose1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.BtnDiscord.BackColor = System.Drawing.Color.Transparent
+        Me.BtnDiscord.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.BtnDiscord.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnDiscord.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.BtnDiscord.Location = New System.Drawing.Point(482, 316)
+        Me.BtnDiscord.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.BtnDiscord.Name = "BtnDiscord"
+        Me.BtnDiscord.Rounded = False
+        Me.BtnDiscord.Size = New System.Drawing.Size(110, 50)
+        Me.BtnDiscord.TabIndex = 6
+        Me.BtnDiscord.Text = "Discord"
+        Me.BtnDiscord.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
         '
-        'FlatMini1
+        'BtnWebSite
         '
-        Me.FlatMini1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.FlatMini1.BackColor = System.Drawing.Color.White
-        Me.FlatMini1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
-        Me.FlatMini1.Font = New System.Drawing.Font("Marlett", 12.0!)
-        Me.FlatMini1.Location = New System.Drawing.Point(746, 12)
-        Me.FlatMini1.Name = "FlatMini1"
-        Me.FlatMini1.Size = New System.Drawing.Size(18, 18)
-        Me.FlatMini1.TabIndex = 1
-        Me.FlatMini1.Text = "FlatMini1"
-        Me.FlatMini1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.BtnWebSite.BackColor = System.Drawing.Color.Transparent
+        Me.BtnWebSite.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.BtnWebSite.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnWebSite.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.BtnWebSite.Location = New System.Drawing.Point(482, 373)
+        Me.BtnWebSite.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.BtnWebSite.Name = "BtnWebSite"
+        Me.BtnWebSite.Rounded = False
+        Me.BtnWebSite.Size = New System.Drawing.Size(110, 50)
+        Me.BtnWebSite.TabIndex = 5
+        Me.BtnWebSite.Text = "Site"
+        Me.BtnWebSite.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
         '
-        'FlatButton1
+        'ProgBarDownloadMod
         '
-        Me.FlatButton1.BackColor = System.Drawing.Color.Transparent
-        Me.FlatButton1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
-        Me.FlatButton1.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.FlatButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.FlatButton1.Location = New System.Drawing.Point(12, 344)
-        Me.FlatButton1.Name = "FlatButton1"
-        Me.FlatButton1.Rounded = False
-        Me.FlatButton1.Size = New System.Drawing.Size(146, 46)
-        Me.FlatButton1.TabIndex = 2
-        Me.FlatButton1.Text = "Mise à jour"
-        Me.FlatButton1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.ProgBarDownloadMod.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.ProgBarDownloadMod.DarkerProgress = System.Drawing.Color.FromArgb(CType(CType(23, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(92, Byte), Integer))
+        Me.ProgBarDownloadMod.Location = New System.Drawing.Point(9, 429)
+        Me.ProgBarDownloadMod.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.ProgBarDownloadMod.Maximum = 100
+        Me.ProgBarDownloadMod.Name = "ProgBarDownloadMod"
+        Me.ProgBarDownloadMod.ProgressColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.ProgBarDownloadMod.Size = New System.Drawing.Size(582, 42)
+        Me.ProgBarDownloadMod.TabIndex = 4
+        Me.ProgBarDownloadMod.Text = "FlatProgressBar1"
+        Me.ProgBarDownloadMod.Value = 50
         '
-        'FlatButton2
+        'BtnPlay
         '
-        Me.FlatButton2.BackColor = System.Drawing.Color.Transparent
-        Me.FlatButton2.BaseColor = System.Drawing.Color.Red
-        Me.FlatButton2.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.FlatButton2.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.FlatButton2.Location = New System.Drawing.Point(12, 292)
-        Me.FlatButton2.Name = "FlatButton2"
-        Me.FlatButton2.Rounded = False
-        Me.FlatButton2.Size = New System.Drawing.Size(146, 46)
-        Me.FlatButton2.TabIndex = 3
-        Me.FlatButton2.Text = "Jouer"
-        Me.FlatButton2.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.BtnPlay.BackColor = System.Drawing.Color.Transparent
+        Me.BtnPlay.BaseColor = System.Drawing.Color.Red
+        Me.BtnPlay.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnPlay.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.BtnPlay.Location = New System.Drawing.Point(9, 316)
+        Me.BtnPlay.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.BtnPlay.Name = "BtnPlay"
+        Me.BtnPlay.Rounded = False
+        Me.BtnPlay.Size = New System.Drawing.Size(110, 50)
+        Me.BtnPlay.TabIndex = 3
+        Me.BtnPlay.Text = "Jouer"
+        Me.BtnPlay.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
         '
-        'FlatProgressBar1
+        'BtnMaj
         '
-        Me.FlatProgressBar1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
-        Me.FlatProgressBar1.DarkerProgress = System.Drawing.Color.FromArgb(CType(CType(23, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(92, Byte), Integer))
-        Me.FlatProgressBar1.Location = New System.Drawing.Point(12, 396)
-        Me.FlatProgressBar1.Maximum = 100
-        Me.FlatProgressBar1.Name = "FlatProgressBar1"
-        Me.FlatProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
-        Me.FlatProgressBar1.Size = New System.Drawing.Size(776, 42)
-        Me.FlatProgressBar1.TabIndex = 4
-        Me.FlatProgressBar1.Text = "FlatProgressBar1"
-        Me.FlatProgressBar1.Value = 50
+        Me.BtnMaj.BackColor = System.Drawing.Color.Transparent
+        Me.BtnMaj.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.BtnMaj.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnMaj.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.BtnMaj.Location = New System.Drawing.Point(9, 373)
+        Me.BtnMaj.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.BtnMaj.Name = "BtnMaj"
+        Me.BtnMaj.Rounded = False
+        Me.BtnMaj.Size = New System.Drawing.Size(110, 50)
+        Me.BtnMaj.TabIndex = 2
+        Me.BtnMaj.Text = "Mise à jour"
+        Me.BtnMaj.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
         '
-        'FlatButton3
+        'BtnMini
         '
-        Me.FlatButton3.BackColor = System.Drawing.Color.Transparent
-        Me.FlatButton3.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
-        Me.FlatButton3.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.FlatButton3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.FlatButton3.Location = New System.Drawing.Point(642, 344)
-        Me.FlatButton3.Name = "FlatButton3"
-        Me.FlatButton3.Rounded = False
-        Me.FlatButton3.Size = New System.Drawing.Size(146, 46)
-        Me.FlatButton3.TabIndex = 5
-        Me.FlatButton3.Text = "Site"
-        Me.FlatButton3.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.BtnMini.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnMini.BackColor = System.Drawing.Color.White
+        Me.BtnMini.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.BtnMini.Font = New System.Drawing.Font("Marlett", 12.0!)
+        Me.BtnMini.Location = New System.Drawing.Point(560, 13)
+        Me.BtnMini.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.BtnMini.Name = "BtnMini"
+        Me.BtnMini.Size = New System.Drawing.Size(18, 18)
+        Me.BtnMini.TabIndex = 1
+        Me.BtnMini.Text = "FlatMini1"
+        Me.BtnMini.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
         '
-        'FlatButton4
+        'BtnClose
         '
-        Me.FlatButton4.BackColor = System.Drawing.Color.Transparent
-        Me.FlatButton4.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
-        Me.FlatButton4.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.FlatButton4.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.FlatButton4.Location = New System.Drawing.Point(642, 292)
-        Me.FlatButton4.Name = "FlatButton4"
-        Me.FlatButton4.Rounded = False
-        Me.FlatButton4.Size = New System.Drawing.Size(146, 46)
-        Me.FlatButton4.TabIndex = 6
-        Me.FlatButton4.Text = "Discord"
-        Me.FlatButton4.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.BtnClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnClose.BackColor = System.Drawing.Color.White
+        Me.BtnClose.BaseColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.BtnClose.Font = New System.Drawing.Font("Marlett", 10.0!)
+        Me.BtnClose.Location = New System.Drawing.Point(578, 13)
+        Me.BtnClose.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(18, 18)
+        Me.BtnClose.TabIndex = 0
+        Me.BtnClose.Text = "FlatClose1"
+        Me.BtnClose.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.FormSkin1)
+        Me.ClientSize = New System.Drawing.Size(600, 488)
+        Me.Controls.Add(Me.BackGround)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Launcher Imperial Life"
         Me.TransparencyKey = System.Drawing.Color.Fuchsia
-        Me.FormSkin1.ResumeLayout(False)
+        Me.BackGround.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents FormSkin1 As FormSkin
-    Friend WithEvents FlatMini1 As FlatMini
-    Friend WithEvents FlatClose1 As FlatClose
-    Friend WithEvents FlatButton1 As FlatButton
-    Friend WithEvents FlatButton2 As FlatButton
-    Friend WithEvents FlatProgressBar1 As FlatProgressBar
-    Friend WithEvents FlatButton3 As FlatButton
-    Friend WithEvents FlatButton4 As FlatButton
+    Friend WithEvents BackGround As FormSkin
+    Friend WithEvents BtnMini As FlatMini
+    Friend WithEvents BtnClose As FlatClose
+    Friend WithEvents BtnMaj As FlatButton
+    Friend WithEvents BtnPlay As FlatButton
+    Friend WithEvents ProgBarDownloadMod As FlatProgressBar
+    Friend WithEvents BtnWebSite As FlatButton
+    Friend WithEvents BtnDiscord As FlatButton
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,14 @@
 ﻿Public Class Form1
-    Private Sub FlatButton3_Click(sender As Object, e As EventArgs) Handles FlatButton3.Click
-
+    Private Sub OnClickBtnWebSite(sender As Object, e As EventArgs) Handles BtnWebSite.Click
+        System.Diagnostics.Process.Start("https://imperial-life.fr/")
     End Sub
+
+    Private Sub OnClickPlay(sender As Object, e As EventArgs) Handles BtnPlay.Click
+        System.Diagnostics.Process.Start("")
+    End Sub
+
+    Private Sub OnClick_BtnDiscord(sender As Object, e As EventArgs) Handles BtnDiscord.Click
+        System.Diagnostics.Process.Start("https://discord.gg/j34MKM94dd")
+    End Sub
+
 End Class
